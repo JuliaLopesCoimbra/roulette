cafe9f4afce41fccacbeacc9489b8e8f19e344aa
unknown <juliacristinalopes2607@gmail.com>
2025-11-03 07:13:20 -0300

    front atualizado

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,11 +22,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
 <head>
+  <meta charSet="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-  <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Dancing+Script:wght@400..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
-  <link rel="icon" href="/img/favicon.ico" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Bangers&family=Dancing+Script:wght@400..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet"
+  />
+
+  <link rel="icon" href="/img/logo/logo_adworld.png" sizes="any" />
+  <link rel="icon" type="image/png" href="/img/favicon.png" />
+  <link rel="apple-touch-icon" href="/img/apple-icon.png" />
+  <link rel="manifest" href="/manifest.json" />
 </head>
+
 <body
   className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 >
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 import { getUserToken } from "./auth";
 import { getClientToken } from "./auth";
-export const URL_BASE = "http://127.0.0.1:8000";
+export const URL_BASE = "http://54.233.30.252:8000";
 
 // utils/api.js
 async function http(path, init = {}) {
