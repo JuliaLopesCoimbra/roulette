97ae16f874a1a1bb900a9efa93abf6e65c751a98
Fernando Almeida da Silva Filho <fernandoasfilho74@gmail.com>
2025-12-05 09:07:49 -0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 "use client";
-import React, { useState } from "react";
+import React, { Suspense } from "react";
 
 import SignIn from "./pages/user/signIn/page";
+
 export default function Home() {
     return (
-        <div >
-         <SignIn />
-        </div>
+        <Suspense fallback={<div className="min-h-screen flex items-center justify-center">Carregando...</div>}>
+            <SignIn />
+        </Suspense>
     );
 }
