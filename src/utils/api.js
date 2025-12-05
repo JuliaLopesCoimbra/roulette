02618c78f0682c965be693660be37eebd0e7b475
JuliaLopesCoimbra <juliacristinalopes2607@gmail.com>
2025-12-05 09:21:06 -0300

    a

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 // utils/api.js
 import { getUserToken } from "./auth";
 import { getClientToken } from "./auth";
-export const URL_BASE = "http://127.0.0.1:8000";
+export const URL_BASE = "http://54.233.31.137:3333";
 
 // utils/api.js
 async function http(path, init = {}) {
